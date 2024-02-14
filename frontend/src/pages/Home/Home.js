07ded3f1796a6b2827af1b73631fcd5af2b34ba7
hulaun <vangolahung4@gmail.com>
2024-02-14 10:22:24 +0700

    create backend

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,26 @@
+import classNames from "classnames/bind";
+import styles from "./Home.module.scss";
+
+const cx = classNames.bind(styles);
+function Home() {
+  var products;
+  return (
+    <div>
+      {/* {
+        products.map(product => {
+
+        })
+      } */}
+      <div className={cx("product")}>
+        <div className={cx("product-image")}>{/* todo */}</div>
+        <div className={cx("product-description")}>
+          <p className={cx("product-name")}></p>
+          <p className={cx("product-price")}></p>
+          <p className={cx("product-purchased")}></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
